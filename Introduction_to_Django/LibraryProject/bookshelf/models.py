390f882abea1b44a3f7e5a7714a46c3ad76a4726
Gidiiketer1 <gideonkipketer789@gmail.com>
2025-08-24 00:50:57 +0300

    ghj

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -3,9 +3,13 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    published_date = models.DateField()  
+    published_date = models.DateField()  # Ensure this field exists
     isbn = models.CharField(max_length=13)
     pages = models.PositiveIntegerField()
 
     def __str__(self):
         return self.title
+
+    @property
+    def publication_year(self):
+        return self.published_date.year if self.published_date else None
