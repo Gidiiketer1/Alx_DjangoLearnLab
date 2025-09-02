92a05a63a22f88c94ed36bbf768a466732c054d1
Gidiiketer1 <gideonkipketer789@gmail.com>
2025-09-02 13:22:19 +0300

    dfdr

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -13,3 +13,24 @@ INSTALLED_APPS = [
 
 # Use the custom user model
 AUTH_USER_MODEL = 'bookshelf.CustomUser'
+DEBUG = False
+
+ALLOWED_HOSTS = ['yourdomain.com', 'localhost', '127.0.0.1']
+
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+# CSP settings using django-csp (install if needed)
+INSTALLED_APPS += ['csp']
+MIDDLEWARE += [
+    'csp.middleware.CSPMiddleware',
+]
+
+CSP_DEFAULT_SRC = ("'self'",)
+CSP_SCRIPT_SRC = ("'self'",)
+CSP_STYLE_SRC = ("'self'",)
+CSP_IMG_SRC = ("'self'", 'data:')
