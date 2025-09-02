84ca92e0707030dff3ba243d7888b0be8a7f85aa
Gidiiketer1 <gideonkipketer789@gmail.com>
2025-09-02 13:43:10 +0300

    d

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,4 +1,3 @@
-from .forms import BookForm, ExampleForm
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
