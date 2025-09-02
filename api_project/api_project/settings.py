aa06c9d1ac3c639339aee2af40107508483777fd
Gidiiketer1 <gideonkipketer789@gmail.com>
2025-09-03 00:21:02 +0300

    api_project/settings.py doesn't contain: ["rest_framework.permissions.IsAuthenticated"]

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -114,3 +114,12 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ]
 }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
