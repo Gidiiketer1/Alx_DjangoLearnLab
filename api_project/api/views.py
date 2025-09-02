ad25c9ce0615758a1d5803ae39baea0723c4c3b5
Gidiiketer1 <gideonkipketer789@gmail.com>
2025-09-02 22:30:38 +0300

    views

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,7 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(APIView):
-    def get(self, request):
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        return Response(serializer.data)
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
