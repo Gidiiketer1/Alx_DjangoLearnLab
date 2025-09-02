778983805e24876617ce74f7b074f7e39132107a
Gidiiketer1 <gideonkipketer789@gmail.com>
2025-09-03 00:34:11 +0300

    models api

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    """
+    Represents an author with a name.
+    """
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    """
+    Represents a book with a title, publication year, and an author.
+    """
+    title = models.CharField(max_length=200)
+    publication_year = models.PositiveIntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
