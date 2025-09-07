6435b73e5a63c09141e07281084b5c56ebadd3f2
Gidiiketer1 <gideonkipketer789@gmail.com>
2025-09-07 07:40:16 +0300

    vsd

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -1,13 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from ..blog import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Admin route
-    path('', include('blog.urls')),   # Include blog app URLs
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
 ]
-
-# Serve static files during development (only in DEBUG mode)
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
