a1a6ebdb6d4423f5f82443ae4ac24d5893007ac8
Gidiiketer1 <gideonkipketer789@gmail.com>
2025-09-07 07:37:12 +0300

    html

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import RegisterView, ProfileView
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('profile/', ProfileView.as_view(), name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
 ]
