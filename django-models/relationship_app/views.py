40edf0dd8cf31a83206fe6831227659c1fea60f0
Gidiiketer1 <155982224+Gidiiketer1@users.noreply.github.com>
2026-02-22 12:02:23 +0300

    Refactor views.py by removing unused imports
    Removed unused import of Library model and cleaned up the code.

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,27 +4,19 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from .models import Library
 from .models import Book
-from .models import Library
 
-# Function-based view: List all books
+
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
 
-# Class-based view: Detail view for a specific library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["books"] = self.object.book_set.all()
-        return context
 
-
-# User registration view
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
