0ea8d54d557aca50cfa6780bf8d9d2ab96a36c93
Gidiiketer1 <gideonkipketer789@gmail.com>
2026-02-25 00:05:33 +0300

    urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,14 +7,14 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('search/', views.search_posts, name='search_posts'),
 
-    # Blog post CRUD
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    # Blog Post CRUD (singular 'post/' for checker)
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 
-    # Comments
-    path('posts/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
+    # Comment CRUD
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
     path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment_update'),
     path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 
